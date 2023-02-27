8e68f4706638af119d2fd6b20810f67183a2360f
osamaelkassaby <osamaelkassaby11@gmail.com>
2023-02-27 22:36:26 +0200

    new

---
+++ Task 1 &2/task.sql
@@ -0,0 +1,5 @@
+SELECT * FROM `products` WHERE price > 4000;
+SELECT * FROM `products` WHERE price > 2000;
+SELECT * FROM `products` WHERE name LIKE 'h%';
+SELECT * FROM `products` WHERE name LIKE '%p';
+
